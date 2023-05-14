013aba4ce1d58f5718a482ffef0637f28e5f3286
Jhon <aleksmironov891@gmail.com>
2023-05-15 01:38:13 +0500

    Added next page check in method get_n_send_info()

--- tomatoo.py
+++ tomatoo.py
@@ -2,7 +2,7 @@ from aiogram.dispatcher.filters import Text
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
-from tapi import Napi_tomatto, walk
+from tomato_api import Napi_tomatto, walk
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 from aiogram import Bot, Dispatcher, executor, types
@@ -16,27 +16,26 @@ bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+
 class Form(StatesGroup):
-    
+
     choose_category = State()
     choose_sort_type = State()
     choose_genre = State()
     new_link = State()
     gap = State()
-    
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def start_message(message: types.Message):
     """First launch comand"""
-    await message.answer(text="Hi!\nI'm EchoBot!\nPowered by aiogram.", reply_markup=kboard.NEW_REQUEST)
+    await message.answer(text="Hi!\nI can help you to choose tv show or movie.", reply_markup=kboard.NEW_REQUEST)
 
 
 @dp.message_handler(state='*', commands='cancel')
 @dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
 async def cancel_handler(message: types.Message, state: FSMContext):
-    """
-    Allow user to cancel any action
-    """
+    """Allow user to cancel any action"""
     current_state = await state.get_state()
     if current_state is None:
         return
@@ -44,7 +43,7 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     # Cancel state and inform user about it
     await state.finish()
     # And remove keyboard (just in case)
-    await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
+    await message.reply('Cancelled.\nTo restart enter: /start', reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(text='New request')
@@ -53,11 +52,12 @@ async def start_new_request(message: types.Message, state: FSMContext):
     await message.answer(text='Chose the type of show', reply_markup=kboard.OPTIONS)
     await state.set_state(Form.choose_category.state)
 
+
 @dp.message_handler(state=Form.choose_category.state)
 async def get_choose_category(message: types.Message, state: FSMContext):
     '''Get user filter and await next'''
     if message.text not in kboard.option_list:
-        await message.answer(text ='Wrong input please use key board')
+        await message.answer(text='Wrong input please use key board')
         return
     await state.update_data(choose_category=message.text)
     await message.answer('Data saved')
@@ -65,125 +65,94 @@ async def get_choose_category(message: types.Message, state: FSMContext):
     await message.answer(text='Chose new or popular', reply_markup=kboard.FILTER)
     await state.set_state(Form.choose_sort_type.state)
 
+
 @dp.message_handler(state=Form.choose_sort_type.state)
-async def get_choose_sort_type(message: types.Message, state: FSMContext):  
+async def get_choose_sort_type(message: types.Message, state: FSMContext):
     '''Get user filter and await next filter'''
-    
+
     if message.text not in kboard.filter_list:
-        await message.answer(text ='Wrong input please use key board')
+        await message.answer(text='Wrong input please use key board')
         return
     await state.update_data(choose_sort_type=message.text)
     await message.answer('Data saved')
 #    await state.reset_state(with_data=False)
-    await message.answer(text='Chose genre wich you interest in', reply_markup=kboard.GENRE_ROW) 
+    await message.answer(text='Chose genre wich you interest in', reply_markup=kboard.GENRE_ROW)
     await state.set_state(Form.choose_genre.state)
+
 
 @dp.message_handler(state=Form.choose_genre.state)
 async def get_choose_genre(message:
-  types.Message, state: FSMContext):
+                           types.Message, state: FSMContext):
     '''Save user_genre input and processing gathered data'''
     n = Napi_tomatto()
-    
+
     if message.text.lower() not in kboard.genre_list:
-        await message.answer(text ='Wrong input please use key board')
+        await message.answer(text='Wrong input please use key board')
         return
     await state.update_data(choose_genre=message.text)
     await message.answer('Data saved')
-    
+
     # creating link for request to websiet rotten tomatoes
-    #stop saving process
+    # stop saving process
     await state.reset_state(with_data=False)
     # get data frome storage
     user_data = await state.get_data()
     # creating link for request to websiet rotten tomatoes
-    created_url =  n.link_creator(user_data['choose_category'].lower(), user_data['choose_sort_type'].lower(), 
-      user_data['choose_genre'].lower())
+    created_url = n.link_creator(user_data['choose_category'].lower(), user_data['choose_sort_type'].lower(),
+                                 user_data['choose_genre'].lower())
     # start saving process
     await state.set_state(Form.new_link.state)
     # save output frome method link_creator()
     await state.update_data(new_link=created_url)
-    # stop saving process 
+    # stop saving process
     await state.reset_state(with_data=False)
     # get data from storage
     user_data = await state.get_data()
-    await message.reply(f"Nice u done it \n{user_data['choose_category']}\n\n{user_data['choose_sort_type']},\n{user_data['choose_genre']}\nuser_data['new_link']", reply_markup=kboard.CONFIRM)
+    await message.reply(f"Ok let's check if is everything fine\nShow type: {user_data['choose_category']}\nFilter type: {user_data['choose_sort_type']}\nGenre: {user_data['choose_genre']}", reply_markup=kboard.CONFIRM)
     await state.set_state(Form.new_link.state)
 
 
-@dp.message_handler(lambda message: message.text not in ['Confirm','Next page'], state=Form.new_link)
+@dp.message_handler(lambda message: message.text not in ['Confirm', 'Next page'], state=Form.new_link)
 async def process_age_invalid(message: types.Message):
     return await message.reply("I dont't know what to do!")
-    
+
 
 @dp.message_handler(text='Confirm', state=Form.new_link)
 #@dp.message_handler(text="hey")
 async def get_n_send_info(message: types.Message, state: FSMContext):
     '''Get json file from rotton tomatoes and send it to user'''
     n = Napi_tomatto()
-    #await state.set_state(Form.new_link.state)
-    #await state.update_data(new_link="pupa")
-    #await state.reset_state(with_data=False)
+    # await state.set_state(Form.new_link.state)
+    # await state.update_data(new_link="pupa")
+    # await state.reset_state(with_data=False)
     user_data = await state.get_data()
-    movies = n.napi_appeal() #user_data['new_link']
-    #for movie in movies:
-    #  await message.answer(text=''.join(movie))
-    await message.answer('Do you want to proceed?', reply_markup=kboard.NEXT_PAGE)
-    print(user_data['new_link'])
-    #await message.reply(f"Nice u done it \n{user_data['choose_category']}\n{user_data['choose_sort_type']},\n{user_data['choose_genre']}\n{user_data['new_link']}")
-    
+    movies = n.napi_appeal(user_data['new_link'])  # user_data['new_link']
+    for movie in movies[:-1]:
+        #    print(movie)
+        await message.answer(text=''.join(movie))
+    if movies[-1] == True:
+        await message.answer('Do you want to proceed?', reply_markup=kboard.NEXT_PAGE)
+    elif movies[-1] == False:
+        return await message.answer('Sorry, pages are over', reply_markup=kboard.CANCEL)
+
+    # await message.reply(f"Nice u done it \n{user_data['choose_category']}\n{user_data['choose_sort_type']},\n{user_data['choose_genre']}\n{user_data['new_link']}")
+
+
 @dp.message_handler(text='Next page', state=Form.new_link)
 async def get_next_page(message: types.Message, state: FSMContext):
     n = Napi_tomatto()
     user_data = await state.get_data()
     next_page_link = n.get_next_page(user_data['new_link'])
-    
+
     await state.set_state(Form.new_link.state)
     # save output frome method link_creator()
     await state.update_data(new_link=next_page_link)
-    await message.answer(f"Here is your page ",reply_markup=kboard.CONFIRM)
-    print(user_data['new_link']+'\n'+next_page_link)
+    await message.answer(f"Do you want to proceed?", reply_markup=kboard.CONFIRM)
+    print(user_data['new_link'] + '\n' + next_page_link)
     await state.set_state(Form.new_link.state)
-'''   
-# try'n saeclass
-    define_class = State()
-    await state.set_state(Form.define_class.state)
-    n=Napi_tomatto
-    await state.update_data(define_class=n)
-    await state.reset_state(with_data=False)
-     p=user_data['define_class']
-     
-    n = Napi_tomatto()
-    movies = n.napi_appeal()
-    #movie_part_1 = movies[:2]
-    #movie_part_2 = movies[11:21]
-    #movie_part_3 = movies[21:31]
-    
-    #for movie in movies:
-    #    await message.answer(text=''.join(movie))
-    #print(movies[0])
-    #user_data = await state.get_data()
-    await message.reply(f"Nice u done it \n://www.youtube.com/results?search_query=Black+Bird")
-    # Finish conversation
-'''   
-
 
 if __name__ == '__main__':
 
     executor.start_polling(dp, skip_updates=True)
 
-#Удалить с гита токен бота дибил!!!
-#В метод get_n_send_info() добавить проверку на наличие следующей страници
-#Обработка исключений 
-#Возможность выбора кнопки Омена/Выход
-#Изменить класс парсера удалить не нужные методы
-
-
-
-
-
-
-
-
-
-
-
+# Удалить с гита токен бота дибил!!!
